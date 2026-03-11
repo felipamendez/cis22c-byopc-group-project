573f1162bab8e0f2779f99519e2b3584e8463a8f
Felipa Mendez <felipalouise@gmail.com>
2026-03-11 16:49:29 -0700

    fleshed out custoemr employee and user

--- user.java
+++ user.java
@@ -1,7 +1,64 @@
-public class User {
+public abstract class User {
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getters
+    public String getFirstName() {
+        return firstName;
+    
+    }
+
+    public String getLastName() {
+        return lastName;
+
+    }
+
+    public String getEmail() {
+        return email;
+
+    }
+
+    public String getPassword() { 
+        return password;
+
+    }
+
+    //setters dont know if well need
+    public void setEmail(String email) {
+        this.email = email;
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
+    }
+
+
+    //other methods
+    public boolean passwordMatch(String pw) { // use for password match at login
+        if (pw.equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override public String toString() {
+        return "First Name: " +  firstName 
+            + "Last Name:  " + lastName 
+            + "email: " + email; // no password yet for security
+    }
+
+
 
 }
